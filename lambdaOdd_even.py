e83467e54c0f9cb209ded9f7fa07426ea4d8b27a
VikramVikrant <mensharetoo@gmail.com>
2020-07-18 18:43:35 +0530

    Lambda function for odd-even

---
+++ lambdaOdd_even.py
@@ -0,0 +1,13 @@
+odd=lambda x:x %2 != 0
+print(f'Is 375 odd? {odd(375)}')
+print(f'Is 678 odd? {odd(678)}')
+even=lambda y:y % 2 == 0
+print(f'Is 456 even? {even(456)}')
+print(f'Is 793 even? {even(793)}')
+
+'''
+Is 375 odd? True
+Is 678 odd? False
+Is 456 even? True
+Is 793 even? False
+'''
