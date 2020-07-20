ccd305f0ee6d731105e6971a43c64658a5dd6f47
VikramVikrant <mensharetoo@gmail.com>
2020-07-20 19:46:28 +0530

    Copying array using typecode

---
+++ copy_array.py
@@ -0,0 +1,8 @@
+from array import *
+value=array('i',[23,45,-89,56,49,90,13])
+print(value.buffer_info())
+#Creating new array
+newArr= array(value.typecode,(a for a in value))
+##We use typecode when we don't know the value we are copying
+for e in newArr:
+    print(e,end=' ')
