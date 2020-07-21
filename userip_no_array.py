b7ad725cb8efd5f3cc51f4f3c97fea94f5692520
VikramVikrant <mensharetoo@gmail.com>
2020-07-21 14:25:53 +0530

    Taking userinput of integers in an array

---
+++ userip_no_array.py
@@ -0,0 +1,10 @@
+from array import *
+element=array('i',[])
+n=int(input("Enter the no. of element:"))
+for i in range(0,n):
+    a=int(input("Enter the next integer:"))
+    element.append(a)
+print(element)
+
+#(15253888, 7)
+#23 45 -89 56 49 90 13
