3af0d4e93742a841f60401b6207cba6604d74d10
VikramVikrant <mensharetoo@gmail.com>
2020-08-10 09:14:03 +0530

    Adding functionalities of filter,map,reduce

---
+++ lambdaprac.py
@@ -0,0 +1,19 @@
+from functools import reduce
+
+no = [2,3,5,7,8,9,11,13,12,23,25,34,36,38]
+even = list(filter(lambda n: n%2 == 0,no))#filter is used to filter the even numbers
+print('list_even_no:',even) #list containing even integers
+
+odd = list(filter(lambda n : n%2 != 0,no))
+print('list_odd_no:',odd) # list containing odd integers
+
+## Map takes some value and it apply some operations to it
+# Here we are doing cube on list of even no.
+cubes =  list(map(lambda n : n**3,even))
+print('Cubes:',cubes)
+
+## If you want add,multiply or any other operations to a large chunk of data we use Reduce
+
+sum = reduce(lambda a,b : a+b, cubes)
+print('Sum of cubes:', sum)
+## For understanding how lambda work with cubes a=8 b=512 then a=520 b=1728 .......
